b06f8c54ad21250dbe14b96eab06cd9f0f674002
Andrew Chen <chen_andrew@berkeley.edu>
2020-07-18 15:01:17 -0700

    type assignment and type inference

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('sddd ssd');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var num1 = 5;
+var num2 = 9;
+var showResult = true;
+var resultPhrase = 'Result is: ';
+add(num1, num2, showResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log('World says hello.')
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+	const result = n1 + n2;
+	if (showResult) {
+		console.log(resultPhrase + result);
+	} else {
+		return result
+	}
+}
+
+let num1: number;
+num1 = 5;
+const num2 = 9;
+const showResult = true;
+const resultPhrase = 'Result is: '
+
+add(num1, num2, showResult, resultPhrase);
+ 
