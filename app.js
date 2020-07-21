bace3d0bf38a9e3c8e1f1e86cfc8ce29c7710fb1
Andrew Chen <chen_andrew@berkeley.edu>
2020-07-21 01:19:47 -0700

    Objects, arrays, tuples, and enum types

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(resultPhrase + result);
-    }
-    else {
-        return result;
-    }
+// const person: {
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	role: [number, string]; // type tuple
+// } = {
+// 	name: "bob",
+// 	age: 7,
+// 	hobbies: ['basketball', 'badminton'],
+// 	role: [2, 'author'] 
+// }
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
+    name: "bob",
+    age: 7,
+    hobbies: ['basketball', 'badminton'],
+    role: Role.ADMIN
+};
+// person.role[1] = 10;
+// person.role.push('admin'); // allowed for tuple
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase()); //TS knows that hobby is a string.
 }
-var num1 = 5;
-var num2 = 9;
-var showResult = true;
-var resultPhrase = 'Result is: ';
-add(num1, num2, showResult, resultPhrase);
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
+}
---
+++ obs-arrays-enums.ts
@@ -0,0 +1,33 @@
+// const person: {
+// 	name: string;
+// 	age: number;
+// 	hobbies: string[];
+// 	role: [number, string]; // type tuple
+// } = {
+// 	name: "bob",
+// 	age: 7,
+// 	hobbies: ['basketball', 'badminton'],
+// 	role: [2, 'author'] 
+// }
+
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+const person = {
+	name: "bob",
+	age: 7,
+	hobbies: ['basketball', 'badminton'],
+	role: Role.ADMIN
+}
+
+// person.role[1] = 10;
+// person.role.push('admin'); // allowed for tuple
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase()); //TS knows that hobby is a string.
+}
+
+if (person.role === Role.ADMIN) {
+	console.log('is admin'); 
+}
